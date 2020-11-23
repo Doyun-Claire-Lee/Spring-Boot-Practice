cf68fa8f5dd7f5f484be000eff9608af70e5b109
Doyun Lee <66059579+Doyun-Claire-Lee@users.noreply.github.com>
2020-11-23 17:33:42 +0900

    SpringOAuth2

--- 4valueAsset/src/main/java/com/test/fourvalueasset/config/SecurityConfig.java
+++ 4valueAsset/src/main/java/com/test/fourvalueasset/config/SecurityConfig.java
@@ -49,8 +49,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .permitAll()
                 .and()
                 .logout();
-//                .and()
-//                .httpBasic();
     }
 
 
